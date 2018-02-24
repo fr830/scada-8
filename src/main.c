868667328a99b0dbbdbc7b33c51cf06d41498abc
mikust <dont@think.so>
2018-02-24 22:52:57 +0200

    RGB led blinking

--- src/main.c
+++ src/main.c
@@ -5,16 +5,23 @@
 
 
 void main(void) {
-    /* Set port B pin 7 for output for Arduino Mega yellow LED */
-    DDRB |= _BV(DDB7);
+    DDRA |= _BV(PA0);
+    DDRA |= _BV(PA2);
+    DDRA |= _BV(PA4);
 
     while (1) {
-        //TODO Student can use bitwise XOR and toggle a bit if such technique is known already.
-        /* Set port B pin 7 high to turn Arduino Mega yellow LED on */
-        PORTB |= _BV(PORTB7);
+        PORTA |= _BV(PA4);
         _delay_ms(BLINK_DELAY_MS);
-        /* Set port B pin 7 high to turn Arduino Mega yellow LED off */
-        PORTB &= ~_BV(PORTB7);
+        PORTA &= ~_BV(PA4);
+        _delay_ms(BLINK_DELAY_MS);
+        PORTA |= _BV(PA2);
+        _delay_ms(BLINK_DELAY_MS);
+        PORTA &= ~_BV(PA2);
+        _delay_ms(BLINK_DELAY_MS);
+        PORTA |= _BV(PA0);
+        _delay_ms(BLINK_DELAY_MS);
+        PORTA &= ~_BV(PA0);
         _delay_ms(BLINK_DELAY_MS);
     }
 }
+
