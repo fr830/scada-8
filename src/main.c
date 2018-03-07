69e0743d5388219393eb7e3f35109b2cd2507c0d
mikust <dont@think.so>
2018-03-07 23:46:32 +0200

    blink_leds() and uart

--- src/main.c
+++ src/main.c
@@ -1,29 +1,38 @@
+#include <stdio.h>
+#define __ASSERT_USE_STDERR
+#include <assert.h>
 #include <avr/io.h>
 #include <util/delay.h>
+#include "uart.h"
 
 #define BLINK_DELAY_MS 100
 
-
-void main(void) {
+static inline void init_leds() {
     DDRA |= _BV(PA0);
     DDRA |= _BV(PA2);
     DDRA |= _BV(PA4);
     DDRB |= _BV(DDB7);
     PORTB &= ~_BV(PORTB7);
+}
 
+static inline void blink_leds(void) {
+    PORTA |= _BV(PA4);
+    _delay_ms(BLINK_DELAY_MS);
+    PORTA &= ~_BV(PA4);
+    _delay_ms(BLINK_DELAY_MS);
+    PORTA |= _BV(PA2);
+    _delay_ms(BLINK_DELAY_MS);
+    PORTA &= ~_BV(PA2);
+    _delay_ms(BLINK_DELAY_MS);
+    PORTA |= _BV(PA0);
+    _delay_ms(BLINK_DELAY_MS);
+    PORTA &= ~_BV(PA0);
+    _delay_ms(BLINK_DELAY_MS);
+}
+
+void main(void) {
     while (1) {
-        PORTA |= _BV(PA4);
-        _delay_ms(BLINK_DELAY_MS);
-        PORTA &= ~_BV(PA4);
-        _delay_ms(BLINK_DELAY_MS);
-        PORTA |= _BV(PA2);
-        _delay_ms(BLINK_DELAY_MS);
-        PORTA &= ~_BV(PA2);
-        _delay_ms(BLINK_DELAY_MS);
-        PORTA |= _BV(PA0);
-        _delay_ms(BLINK_DELAY_MS);
-        PORTA &= ~_BV(PA0);
-        _delay_ms(BLINK_DELAY_MS);
+        blink_leds();
     }
 }
 
--- src/uart.c
+++ src/uart.c
@@ -10,31 +10,62 @@
 #endif
 #include <util/setbaud.h>
 
-/* http://www.cs.mun.ca/~rod/Winter2007/4723/notes/serial/serial.html */
 
-void uart_init(void) {
+void simple_uart0_init(void) {
     UBRR0H = UBRRH_VALUE;
     UBRR0L = UBRRL_VALUE;
-    
 #if USE_2X
     UCSR0A |= _BV(U2X0);
 #else
     UCSR0A &= ~(_BV(U2X0));
 #endif
-
-    UCSR0C = _BV(UCSZ01) | _BV(UCSZ00); /* 8-bit data */ 
-    UCSR0B = _BV(RXEN0) | _BV(TXEN0);   /* Enable RX and TX */    
+    UCSR0C = _BV(UCSZ01) | _BV(UCSZ00); /* 8-bit data */
+    UCSR0B = _BV(RXEN0) | _BV(TXEN0);   /* Enable RX and TX */
+    UCSR0C &= ~_BV(USBS0);               /* 1 stop bit*/
+    UCSR0C &= ~_BV(UPM01) & ~_BV(UPM00); /* no parity bit */
 }
 
-void uart_putchar(char c, FILE *stream) {
+void simple_uart1_init(void) {
+    UBRR1H = UBRRH_VALUE;
+    UBRR1L = UBRRL_VALUE;
+#if USE_2X
+    UCSR1A |= _BV(U2X1);
+#else
+    UCSR1A &= ~(_BV(U2X1));
+#endif
+    UCSR1C = _BV(UCSZ11) | _BV(UCSZ10); /* 8-bit data */
+    UCSR1B = _BV(TXEN1);   /* Enable only TX */
+    UCSR1C &= ~_BV(USBS1);               /* 1 stop bit*/
+    UCSR1C &= ~_BV(UPM11) & ~_BV(UPM10); /* no parity bit */
+}
+
+int simple_uart0_putchar(char c, FILE *stream) {
+    (void) stream;
+
     if (c == '\n') {
-        uart_putchar('\r', stream);
+        simple_uart0_putchar('\r', stream);
     }
+
     loop_until_bit_is_set(UCSR0A, UDRE0);
     UDR0 = c;
+    return (0);
 }
 
-char uart_getchar(FILE *stream) {
+int simple_uart0_getchar(FILE *stream) {
+    (void) stream;
     loop_until_bit_is_set(UCSR0A, RXC0);
     return UDR0;
 }
+
+int simple_uart1_putchar(char c, FILE *stream) {
+    (void) stream;
+
+    if (c == '\n') {
+        simple_uart1_putchar('\r', stream);
+    }
+
+    loop_until_bit_is_set(UCSR1A, UDRE1);
+    UDR1 = c;
+    return (0);
+}
+
